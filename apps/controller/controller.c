c827213a23ac03aca6df9c83304fedcf99544651
kaizsv <kai.zsv@gmail.com>
2017-04-09 18:10:37 +0800

    get controller ip

--- apps/controller/controller.c
+++ apps/controller/controller.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include "freertps/freertps.h"
+#include "controller/helper.h"
 
 int main(int argc, char **argv)
 {
-	printf("hello world\r\n");
+	char *cid = get_controller_ip();
+	if (cid == NULL) {
+		fprintf(stderr, "The controller has no IP, please check again\n");
+		exit(-1);
+	}
+
 	return 0;
 }
